d33b310826a7d8a7117442a04b528ddb9f54d2aa
makuwin <106814267+makuwin@users.noreply.github.com>
2023-10-25 15:38:53 +0900

    small edit

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,12 +1,13 @@
 import { NavLink } from "@mantine/core";
-import { IconHome2 } from "@tabler/icons-react";
+import { IconBuildingStore, IconBuildingWarehouse, IconHome2 } from "@tabler/icons-react";
 import { Link} from "react-router-dom";
 
 export default function NavBar() {
   return (
     <>
       <NavLink label="Home" leftSection={<IconHome2 size="1rem" stroke={1.5} />} component={Link} to={'/'}/>
-      <NavLink label="Inventory" leftSection={<IconHome2 size="1rem" stroke={1.5} />} component={Link} to={'/inventory'}/>
+      <NavLink label="POS" leftSection={<IconBuildingStore size="1rem" stroke={1.5} />} component={Link} to={'/inventory'}/>
+      <NavLink label="Inventory" leftSection={<IconBuildingWarehouse size="1rem" stroke={1.5} />} component={Link} to={'/inventory'}/>
     </>
   )
 }
